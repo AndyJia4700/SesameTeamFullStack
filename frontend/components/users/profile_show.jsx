8f0e26f3d372fe75a7eecdbcb5fbc4c9079a4fbe
andy <jia470079834@gmail.com>
2020-10-05 22:09:50 -0400

    css

--- frontend/components/users/profile_show.jsx
+++ frontend/components/users/profile_show.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { fetchUsers } from '../../actions/user_action';
+import { FiEdit } from "react-icons/fi";
+
 
 const mSTP = (state, ownProps) => {
     // debugger;
@@ -31,51 +33,65 @@ class ProfileShow extends React.Component{
     }
     
     render(){
-        // debugger;
         const { user } = this.props;
 
-        const age = (birthDate, otherDate) => {
-            birthDate = new Date(birthDate);
-            otherDate = new Date(otherDate);
-
-            var years = otherDate.getFullYear() - birthDate.getFullYear();
-
+        const age = (date) => {
+            if(date === null) return 0;
+            const birthDate = new Date(date);
+            const today = new Date();
+            let years = today.getFullYear() - birthDate.getFullYear();
             if (
-                otherDate.getMonth() < birthDate.getMonth() ||
-                (otherDate.getMonth() == birthDate.getMonth() &&
-                otherDate.getDate() < birthDate.getDate())
+                today.getMonth() < birthDate.getMonth() ||
+                (today.getMonth() == birthDate.getMonth() &&
+                today.getDate() < birthDate.getDate())
             ) {
                 years--;
             }
-            return years;
+                return years;
         }
 
-        debugger;
         return (
           <div className="profile-show-main-div">
-            <Link to={`/users/${user.id}/edit`}>Eidt Profile</Link>
+            <h3 className="profile-edit">
+              <Link to={`/users/${user.id}/edit`}>
+                <FiEdit />
+              </Link>
+            </h3>
+
             <div className="profile-photo-div">
               <img src={user.photoUrl} className="profile-photo-img" />
 
               <h3>
                 <input
-                //   placeholder="firstname"
                   value={user.first_name + "  " + user.last_name}
                   className="profile-element"
                   readOnly={true}
                 />
               </h3>
 
-              {user.birthdate}
-              {user.location}
+              <div className="profile-age">
+                  {age(user.birthdate) + " years old"}
+              </div>
             </div>
 
+            {/* {user.location} */}
+
             <ul>
-              <li>Education: {user.education}</li>
-              <li>Skill[]: {user.skill}</li>
-              <li>Personality[]: {user.personality}</li>
-              <li>Interest[]: {user.interest}</li>
-              <li>About: {user.about}</li>
+                <li>
+                  Education: {user.education}
+                </li>
+                <li>
+                    Skill[]: {user.skill}
+                </li>
+                <li>
+                    Personality[]: {user.personality}
+                </li>
+                <li>
+                    Interest[]: {user.interest}
+                </li>
+                <li>
+                    About: {user.about}
+                </li>
             </ul>
           </div>
         );
